e0bee078201f64d06303d8755dd3a86a68b3138b
Meadowcottage <bendixon50@gmail.com>
2015-03-06 23:27:30 +0000

    Initial Code commit

---
+++ src/main/java/com/meadowcottage/Playground/common/Playground.java
@@ -0,0 +1,10 @@
+package com.meadowcottage.Playground.common;
+
+import com.meadowcottage.Playground.common.reference.Reference;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class Playground
+{
+
+}
---
+++ src/main/java/com/meadowcottage/Playground/common/reference/Reference.java
@@ -0,0 +1,8 @@
+package com.meadowcottage.Playground.common.reference;
+
+public class Reference
+{
+	public static final String MODID = "Playground";
+	public static final String NAME = "Playground";
+	public static final String VERSION = "ALPHA-1.0.0";
+}
