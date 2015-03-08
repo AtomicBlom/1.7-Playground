b19e4dcd70a285713415b23313a5dd81bdca626c
Steven Blom <atomicblom@binaryvibrance.net>
2015-03-08 20:34:58 +1030

    One side of connected textures

--- src/main/java/com/meadowcottage/Playground/client/render/ConnectedTextureISBRH.java
+++ src/main/java/com/meadowcottage/Playground/client/render/ConnectedTextureISBRH.java
@@ -5,10 +5,14 @@ import com.meadowcottage.Playground.common.util.textures.TextureDirection;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.util.ForgeDirection;
+import org.lwjgl.util.vector.Vector2f;
 
 public class ConnectedTextureISBRH implements ISimpleBlockRenderingHandler
 {
@@ -33,6 +37,24 @@ public class ConnectedTextureISBRH implements ISimpleBlockRenderingHandler
             return false;
         }
 
+        Tessellator tessellator = Tessellator.instance;
+        int colourMultiplier = block.colorMultiplier(renderer.blockAccess, x, y, z);
+        float red = (float)(colourMultiplier >> 16 & 255) / 255.0F;
+        float green = (float)(colourMultiplier >> 8 & 255) / 255.0F;
+        float blue = (float)(colourMultiplier & 255) / 255.0F;
+
+        if (EntityRenderer.anaglyphEnable)
+        {
+            float adjustedRed = (red * 30.0F + green * 59.0F + blue * 11.0F) / 100.0F;
+            float adjustedGreen = (red * 30.0F + green * 70.0F) / 100.0F;
+            float adjustedBlue = (red * 30.0F + blue * 70.0F) / 100.0F;
+            red = adjustedRed;
+            green = adjustedGreen;
+            blue = adjustedBlue;
+        }
+
+        tessellator.setColorOpaque_F(red, green, blue);
+
         IConnectedTextureBlock ctBlock = (IConnectedTextureBlock)block;
 
         for (ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
@@ -41,102 +63,142 @@ public class ConnectedTextureISBRH implements ISimpleBlockRenderingHandler
             }
         }
 
-        return false;
+        return true;
     }
 
     private void renderSide(IBlockAccess world, int x, int y, int z, ForgeDirection side, IConnectedTextureBlock block, RenderBlocks renderer)
     {
-        final ForgeDirection belowDirection = BlockSideRotation.forOrientation(TextureDirection.BELOW, side);
-        ForgeDirection[] verticalDirections = {
-                BlockSideRotation.forOrientation(TextureDirection.ABOVE, side),
-                belowDirection
+        if (side != ForgeDirection.SOUTH) return;
+
+        Tessellator tessellator = Tessellator.instance;
+
+        TextureQuadrant[] quadrants = {
+                new TextureQuadrant(TextureDirection.LEFT, TextureDirection.ABOVE, side),
+                new TextureQuadrant(TextureDirection.RIGHT, TextureDirection.ABOVE, side),
+                new TextureQuadrant(TextureDirection.LEFT, TextureDirection.BELOW, side),
+                new TextureQuadrant(TextureDirection.RIGHT, TextureDirection.BELOW, side),
         };
-        final ForgeDirection rightDirection = BlockSideRotation.forOrientation(TextureDirection.RIGHT, side);
-        ForgeDirection[] horizontalDirections = {
-                BlockSideRotation.forOrientation(TextureDirection.LEFT, side),
-                rightDirection
-        };
+
 
         TextureCoordinates coordinates;
-        for (ForgeDirection horizontalDirection : horizontalDirections) {
-            for (ForgeDirection verticalDirection : verticalDirections) {
-                coordinates = getSubTextureForBlock(world, x, y, z, side.ordinal(), block,
-                        horizontalDirection, verticalDirection,
-                        horizontalDirection == rightDirection,
-                        verticalDirection == belowDirection
+        for (final TextureQuadrant quadrant : quadrants)
+        {
+            coordinates = getSubTextureForBlock(world, x, y, z, block, quadrant);
 
-                );
+            tessellator.addVertexWithUV(x + quadrant.tuStart, y + quadrant.tvStart, z, coordinates.tuFrom, coordinates.tvFrom);
+            tessellator.addVertexWithUV(x + quadrant.tuStart, y + quadrant.tvEnd, z, coordinates.tuFrom, coordinates.tvTo);
+            tessellator.addVertexWithUV(x + quadrant.tuEnd, y + quadrant.tvEnd, z, coordinates.tuTo, coordinates.tvTo);
+            tessellator.addVertexWithUV(x + quadrant.tuEnd, y + quadrant.tvStart, z, coordinates.tuTo, coordinates.tvFrom);
+
+
+        }
+    }
+
+    private static class TextureQuadrant {
+        final ForgeDirection side;
+        final ForgeDirection horizontalDirection;
+        final ForgeDirection verticalDirection;
+        final boolean offsetVerticalTexture;
+        final boolean offsetHorizontalTexture;
+
+        float tuStart = 0.0f;
+        float tuEnd = 0.5f;
+        float tvStart = 0.0f;
+        float tvEnd = 0.5f;
+
+        public TextureQuadrant(TextureDirection horizontal, TextureDirection vertical, ForgeDirection side)
+        {
+            this.side = side;
+            this.horizontalDirection = BlockSideRotation.forOrientation(horizontal, side);
+            this.verticalDirection = BlockSideRotation.forOrientation(vertical, side);
+
+            offsetHorizontalTexture = horizontal == TextureDirection.LEFT;
+            offsetVerticalTexture = vertical == TextureDirection.ABOVE;
+
+            if (offsetHorizontalTexture) {
+                tuStart += 0.5;
+                tuEnd += 0.5;
+            }
+            if (offsetVerticalTexture) {
+                tvStart += 0.5;
+                tvEnd += 0.5;
             }
         }
     }
 
-    private TextureCoordinates getSubTextureForBlock(IBlockAccess world, int x, int y, int z, int side, IConnectedTextureBlock block,
-                                                     ForgeDirection horizontalDirection, ForgeDirection verticalDirection,
-                                                     boolean offsetHorizontalTexture, boolean offsetVerticalTexture)
+    private TextureCoordinates getSubTextureForBlock(IBlockAccess world, int x, int y, int z, IConnectedTextureBlock block, TextureQuadrant quadrant)
     {
-        TextureCoordinates coordinates = new TextureCoordinates();
+        TextureCoordinates  coordinates = new TextureCoordinates();
         int metadata = world.getBlockMetadata(x, y, z);
         boolean horizontalConnected = block == world.getBlock(
-                x + horizontalDirection.offsetX,
-                y + horizontalDirection.offsetY,
-                z + horizontalDirection.offsetZ);
+                x + quadrant.horizontalDirection.offsetX,
+                y + quadrant.horizontalDirection.offsetY,
+                z + quadrant.horizontalDirection.offsetZ);
         boolean verticalConnected = block == world.getBlock(
-                x + verticalDirection.offsetX,
-                y + verticalDirection.offsetY,
-                z + verticalDirection.offsetZ);
+                x + quadrant.verticalDirection.offsetX,
+                y + quadrant.verticalDirection.offsetY,
+                z + quadrant.verticalDirection.offsetZ);
         boolean cornerConnected = block == world.getBlock(
-                x + horizontalDirection.offsetX + verticalDirection.offsetX,
-                y + horizontalDirection.offsetY + verticalDirection.offsetY,
-                z + horizontalDirection.offsetZ + verticalDirection.offsetZ);
+                x + quadrant.horizontalDirection.offsetX + quadrant.verticalDirection.offsetX,
+                y + quadrant.horizontalDirection.offsetY + quadrant.verticalDirection.offsetY,
+                z + quadrant.horizontalDirection.offsetZ + quadrant.verticalDirection.offsetZ);
 
+        int uStart = 0;
+        int vStart = 0;
+        int subIconsPerIcon;
+        IIcon icon;
+
+        //Use the default texture, both borders
         if (!horizontalConnected && !verticalConnected && !cornerConnected) {
-            final IIcon icon = block.getIcon(side, metadata);
-            coordinates.tuFrom = icon.getMinU();
-            coordinates.tvFrom = icon.getMinV();
-
-            coordinates.tuTo = icon.getMaxU();
-            coordinates.tvTo = icon.getMaxV();
+            icon = block.getIcon(quadrant.side.ordinal(), metadata);
+            subIconsPerIcon = 2;
         } else
         {
-            final IIcon icon = block.getConnectedTextureIcon(side, metadata);
-
-            float minU = icon.getMinU();
-            float minV = icon.getMinV();
-            float maxU = icon.getMaxU();
-            float maxV = icon.getMaxV();
-
-            int uStart = 0;
-            int vStart = 0;
+            //Use the -CTM.png file
+            icon = block.getConnectedTextureIcon(quadrant.side.ordinal(), metadata);
+            subIconsPerIcon = 4;
 
             if (!verticalConnected && horizontalConnected)
             {
                 vStart |= 2;
-            } else if (verticalConnected && !horizontalConnected)
+            }
+            if (verticalConnected && !horizontalConnected)
             {
                 uStart |= 2;
-            } else if (!verticalConnected && !horizontalConnected && cornerConnected)
+            }
+            if (verticalConnected && horizontalConnected && !cornerConnected)
             {
                 uStart |= 2;
                 vStart |= 2;
             }
-
-            if (offsetHorizontalTexture)
-            {
-                uStart |= 1;
-            }
-            if (offsetVerticalTexture)
-            {
-                vStart |= 1;
-            }
-
-            final float textureSizeU = (maxU - minU) / 4;
-            final float textureSizeV = (maxV - minV) / 4;
-            coordinates.tuFrom = textureSizeU * (1.0f / uStart);
-            coordinates.tvFrom = textureSizeV * (1.0f / vStart);
-
-            coordinates.tuTo = coordinates.tuFrom + textureSizeU;
-            coordinates.tvTo = coordinates.tvFrom + textureSizeV;
         }
+
+        if (quadrant.offsetHorizontalTexture)
+        {
+            uStart |= 1;
+        }
+        if (quadrant.offsetVerticalTexture)
+        {
+            vStart |= 1;
+        }
+
+        float minU = icon.getMinU();
+        float minV = icon.getMinV();
+        float maxU = icon.getMaxU();
+        float maxV = icon.getMaxV();
+
+        final float textureSizeU = (maxU - minU);
+        final float textureSizeV = (maxV - minV);
+        final float localTUFrom = (uStart / (float)subIconsPerIcon);
+        final float localTVFrom = (vStart / (float)subIconsPerIcon);
+        final float localTUTo = localTUFrom + (1.0f / subIconsPerIcon);
+        final float localTVTo = localTVFrom + (1.0f / subIconsPerIcon);
+
+        coordinates.tuFrom = minU + textureSizeU * localTUFrom;
+        coordinates.tvFrom = minV + textureSizeV * localTVFrom;
+        coordinates.tuTo = minU + textureSizeU * localTUTo;
+        coordinates.tvTo = minV + textureSizeV * localTVTo;
+
         return coordinates;
     }
 
--- src/main/java/com/meadowcottage/Playground/common/util/textures/BlockSideRotation.java
+++ src/main/java/com/meadowcottage/Playground/common/util/textures/BlockSideRotation.java
@@ -7,8 +7,8 @@ public class BlockSideRotation
     private static final ForgeDirection[][] ROTATION_MATRIX = {
             {ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN},
             {ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN, ForgeDirection.UNKNOWN},
-            {ForgeDirection.EAST, ForgeDirection.WEST, ForgeDirection.NORTH, ForgeDirection.SOUTH, ForgeDirection.DOWN, ForgeDirection.UP, ForgeDirection.UNKNOWN},
-            {ForgeDirection.WEST, ForgeDirection.EAST, ForgeDirection.SOUTH, ForgeDirection.NORTH, ForgeDirection.DOWN, ForgeDirection.UP, ForgeDirection.UNKNOWN},
+            {ForgeDirection.WEST, ForgeDirection.EAST, ForgeDirection.NORTH, ForgeDirection.SOUTH, ForgeDirection.DOWN, ForgeDirection.UP, ForgeDirection.UNKNOWN},
+            {ForgeDirection.EAST, ForgeDirection.WEST, ForgeDirection.SOUTH, ForgeDirection.NORTH, ForgeDirection.DOWN, ForgeDirection.UP, ForgeDirection.UNKNOWN},
             {ForgeDirection.NORTH, ForgeDirection.SOUTH, ForgeDirection.WEST, ForgeDirection.EAST, ForgeDirection.DOWN, ForgeDirection.UP, ForgeDirection.UNKNOWN},
             {ForgeDirection.SOUTH, ForgeDirection.NORTH, ForgeDirection.EAST, ForgeDirection.WEST, ForgeDirection.DOWN, ForgeDirection.UP, ForgeDirection.UNKNOWN}
     };
